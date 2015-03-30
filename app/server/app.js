937e2bcfe7adf1122f111ac3fd67bb1326620c2e
Jacob Klapwijk <hello@jcbk.me>
2015-03-30 22:55:32 +0200

    add another card

--- app/server/app.js
+++ app/server/app.js
@@ -3,6 +3,7 @@
 Meteor.startup(function () {
   if (Cards.find().count() === 0) {
     var items = [
+      {title: "Reduce file size", description: "The app is over 6mb right now, that isn't fun on a 3g connection. We need to reduce that.", order: 0.5, column: 1},
       {title: "Test on Internet Explorer", description: "We haven't tested the app on IE yet. We also need to decide how which IE versions we will support.", order: 1, column: 1},
       {title: "User-defined columns", description: "A cool feature would be to let users define their own columns, instead of being stuck with idea/doing/done", order: 2, column: 1},
       {title: "Inline editing", description: "after creating a card the user should be able to edit the title and description, preferably inline", order: 1, column: 2},
